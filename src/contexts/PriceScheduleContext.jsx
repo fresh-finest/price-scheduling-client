42186a954ed294d23e90298361204c60862cc8df
fresh-finest <bb@brecx.com>
2024-08-07 17:42:14 +0600

    dynamic the context

--- src/contexts/PriceScheduleContext.jsx
+++ src/contexts/PriceScheduleContext.jsx
@@ -1,13 +1,36 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useState, useEffect } from 'react';
+import axios from 'axios';
 
 export const PriceScheduleContext = createContext();
+
+const BASE_URL = 'http://localhost:3000';
 
 export const PriceScheduleProvider = ({ children }) => {
   const [events, setEvents] = useState([]);
 
+  const fetchEvents = async () => {
+    try {
+      const response = await axios.get(`${BASE_URL}/api/schedule`);
+      console.log("schedule data:"+response)
+      const schedules = response.data.result.map(schedule => ({
+        title: `SKU: ${schedule.sku} - $${schedule.price}`,
+        start: new Date(schedule.startDate),
+        end: new Date(schedule.endDate),
+        allDay: false
+      }));
+      setEvents(schedules);
+    } catch (error) {
+      console.error('Error fetching events:', error.response ? error.response.data : error.message);
+    }
+  };
+
   const addEvent = (event) => {
     setEvents((prevEvents) => [...prevEvents, event]);
   };
+
+  useEffect(() => {
+    fetchEvents();
+  }, []);
 
   return (
     <PriceScheduleContext.Provider value={{ events, addEvent }}>
