1a880ab2fca26142cf7389ab8cb3486233f62322
fresh-finest <bb@brecx.com>
2024-08-09 19:19:19 +0600

    react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
-import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Header from "./components/shared/Header";
 import Calendar from './pages/Calendar';
+import List from './pages/List';
+// import Login from './pages/Login';
 import { PriceScheduleProvider } from './contexts/PriceScheduleContext';
 
 function App() {
   return (
     <PriceScheduleProvider>
-      <Header />
-      <Calendar />
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Calendar/>}/>
+          <Route path="/list" element={<List />} />
+          <Route path="/calendar" element={<Calendar />} />
+          {/* <Route path="/login" element={<Login />} /> */}
+        </Routes>
+      </Router>
     </PriceScheduleProvider>
   );
 }
