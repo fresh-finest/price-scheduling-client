e838a2cc8715bf6444dbb41441d034d1e87a3698
fresh-finest <bb@brecx.com>
2024-08-09 19:18:58 +0600

    added router dom

--- src/components/shared/Header.jsx
+++ src/components/shared/Header.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
 import { Navbar, Nav, Container } from 'react-bootstrap';
+import { NavLink } from 'react-router-dom';
 
 function NavScrollExample() {
   return (
@@ -9,11 +9,11 @@ function NavScrollExample() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#list" style={{ color: 'white' }}>List</Nav.Link>
-            <Nav.Link href="#calendar" style={{ color: 'white' }}>Calendar View</Nav.Link>
+            <Nav.Link as={NavLink} to="/list" style={{ color: 'white' }}>List</Nav.Link>
+            <Nav.Link as={NavLink} to="/calendar" style={{ color: 'white' }}>Calendar View</Nav.Link>
           </Nav>
           <Nav>
-            <Nav.Link href="#login" style={{ color: 'white' }}>Logout</Nav.Link>
+            <Nav.Link as={NavLink} to="/login" style={{ color: 'white' }}>Logout</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
