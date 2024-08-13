86e4c33bb70991ee7414a54c49f3588096bcde1d
fresh-finest <bb@brecx.com>
2024-08-13 17:38:53 +0600

    private route set

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,32 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
-import Header from "./components/shared/Header";
 import Calendar from './pages/Calendar';
 import List from './pages/List';
 // import Login from './pages/Login';
 import { PriceScheduleProvider } from './contexts/PriceScheduleContext';
 import Login from './pages/Auth/Login';
+import PrivateRoute from './pages/Auth/PrivateRoute';
 
 function App() {
   return (
     <PriceScheduleProvider>
       <Router>
-        <Header />
+        
         <Routes>
           <Route path="/" element={<Login/>}/>
-          <Route path="/list" element={<List />} />
-          <Route path="/calendar" element={<Calendar />} />
+
+          <Route path="/list" 
+          element={
+          <PrivateRoute>
+            <List />
+          </PrivateRoute>
+          } />
+          <Route path="/calendar"
+           element={
+          <PrivateRoute>
+             <Calendar />
+          </PrivateRoute>
+           } />
           {/* <Route path="/login" element={<Login />} /> */}
         </Routes>
       </Router>
