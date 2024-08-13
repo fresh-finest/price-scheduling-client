e780f5a7d6137600d7dd0847d5bf3118e7d13fc6
fresh-finest <bb@brecx.com>
2024-08-13 17:39:37 +0600

    header imported

--- src/pages/Calendar.jsx
+++ src/pages/Calendar.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import CalendarView from '../components/Calendar/CalendarView'
-
+import Header from '../components/shared/Header'
 export default function Calendar() {
   return (
     <div>
+    <Header/>
         <CalendarView/>
     </div>
   )
