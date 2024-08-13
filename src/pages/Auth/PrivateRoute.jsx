c9f9a0224a28aae9df308dc753fa1edd50c444b1
fresh-finest <bb@brecx.com>
2024-08-13 17:40:28 +0600

    implemeted private route

---
+++ src/pages/Auth/PrivateRoute.jsx
@@ -0,0 +1,12 @@
+// src/components/PrivateRoute.jsx
+import React from 'react';
+import { Navigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+
+const PrivateRoute = ({ children }) => {
+  const { currentUser } = useSelector((state) => state.user);
+
+  return currentUser ? children : <Navigate to="/" />;
+};
+
+export default PrivateRoute;
