cfff62d712377b92cc104988ffef5731d1897c6d
fresh-finest <bb@brecx.com>
2024-08-15 20:46:39 +0600

    wild

--- src/components/List/ProductDetailView.jsx
+++ src/components/List/ProductDetailView.jsx
@@ -35,31 +35,33 @@ const ProductDetailView = ({ product }) => {
   };
 
   return (
-    <Card style={detailStyles.card}>
-      <Card.Img variant="top" src={product.AttributeSets[0].SmallImage.URL} style={detailStyles.image} />
+   <div style={{position:'fixed', width:"450px"}}>
+     <Card style={detailStyles.card}>
+      <Card.Img variant="top" src={product?.AttributeSets[0]?.SmallImage?.URL} style={detailStyles.image} />
       <Card.Body>
-        <Card.Title style={detailStyles.title}>{product.AttributeSets[0].Title}</Card.Title>
+        <Card.Title style={detailStyles.title}>{product?.AttributeSets[0]?.Title}</Card.Title>
         <Card.Text style={detailStyles.info}>
-          <strong>SKU:</strong> {product.AttributeSets[0].Model}
+          <strong>SKU:</strong> {product?.AttributeSets[0]?.Model}
         </Card.Text>
         <Card.Text style={detailStyles.info}>
-          <strong>ASIN:</strong> {product.Identifiers.MarketplaceASIN.ASIN}
+          <strong>ASIN:</strong> {product?.Identifiers?.MarketplaceASIN.ASIN}
         </Card.Text>
         <Card.Text style={detailStyles.info}>
-          <strong>Price:</strong> ${product.AttributeSets[0].ListPrice.Amount}
+          <strong>Price:</strong> ${product?.AttributeSets[0]?.ListPrice.Amount}
         </Card.Text>
         {/* Additional Information */}
         <Card.Text style={detailStyles.info}>
-          <strong>Brand:</strong> {product.AttributeSets[0].Brand}
+          <strong>Brand:</strong> {product?.AttributeSets[0]?.Brand}
         </Card.Text>
         <Card.Text style={detailStyles.info}>
-          <strong>Manufacturer:</strong> {product.AttributeSets[0].Manufacturer}
+          <strong>Manufacturer:</strong> {product?.AttributeSets[0]?.Manufacturer}
         </Card.Text>
         <Card.Text style={detailStyles.info}>
-          <strong>Ranking:</strong> #{product.SalesRankings[0].Rank} in {product.SalesRankings[0].ProductCategoryId}
+          <strong>Ranking:</strong> #{product?.SalesRankings[0]?.Rank} in {product.SalesRankings[0].ProductCategoryId}
         </Card.Text>
       </Card.Body>
     </Card>
+   </div>
   );
 };
 
