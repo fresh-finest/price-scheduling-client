6750d03fab71ef00a156c95080a0ffba9f14c451
fresh-finest <bb@brecx.com>
2024-08-20 18:09:09 +0600

    filtered by user

--- src/components/List/HistoryView.jsx
+++ src/components/List/HistoryView.jsx
@@ -1,37 +1,61 @@
 import React, { useState, useEffect } from "react";
-import { Table, Form, InputGroup, Spinner, Container } from "react-bootstrap";
+import { Table, Form, InputGroup, Spinner, Container, Row, Col } from "react-bootstrap";
 import axios from "axios";
 
-
 const BASE_URL = 'https://dps-server-b829cf5871b7.herokuapp.com';
+// const BASE_URL = 'http://localhost:3000';
 
 
 export default function HistoryView() {
   const [data, setData] = useState([]);
+  const [users, setUsers] = useState([]);
+  const [selectedUser, setSelectedUser] = useState("");
   const [searchTerm, setSearchTerm] = useState("");
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
   useEffect(() => {
-    const fetchData = async () => {
+    const fetchUsers = async () => {
       try {
-        const response = await axios.get(`${BASE_URL}/api/schedule`);
+        const response = await axios.get(`${BASE_URL}/api/user`);
+        setUsers(response.data.result); // Assuming the response is an array of users
+      } catch (err) {
+        console.error("Error fetching users:", err);
+      }
+    };
+
+    fetchUsers();
+  }, []);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const url = selectedUser
+          ? `${BASE_URL}/api/schedule/${selectedUser}/list`
+          : `${BASE_URL}/api/schedule`;
+        const response = await axios.get(url);
         setData(response.data.result);
-        setLoading(false);
       } catch (err) {
         setError(
           "Error fetching data: " +
             (err.response ? err.response.data.message : err.message)
         );
+      } finally {
         setLoading(false);
       }
     };
 
     fetchData();
-  }, []);
+  }, [selectedUser]);
 
+  console.log(selectedUser);
   const handleSearch = (e) => {
     setSearchTerm(e.target.value);
+  };
+
+  const handleUserChange = (e) => {
+    setSelectedUser(e.target.value);
   };
 
   const filteredData = data.filter(
@@ -50,14 +74,29 @@ export default function HistoryView() {
 
   return (
     <Container style={{ marginTop: "100px" }}>
-      <InputGroup className="mb-3" style={{ maxWidth: "300px" }}>
-        <Form.Control
-          type="text"
-          placeholder="Search by ASIN or SKU..."
-          value={searchTerm}
-          onChange={handleSearch}
-        />
-      </InputGroup>
+      <Row className="mb-3">
+        <Col md={3}>
+          <InputGroup>
+            <Form.Control
+              type="text"
+              placeholder="Search by ASIN or SKU..."
+              value={searchTerm}
+              onChange={handleSearch}
+             
+            />
+          </InputGroup>
+        </Col>
+        <Col md={3} className="text-right">
+          <Form.Control as="select" value={selectedUser} onChange={handleUserChange}>
+            <option value="">All Users</option>
+            {users.map((user) => (
+              <option key={user._id} value={user.userName}>
+                Price Updated By {user.userName}
+              </option>
+            ))}
+          </Form.Control>
+        </Col>
+      </Row>
       <Table bordered hover responsive>
         <thead style={{ backgroundColor: "#f0f0f0" }}>
           <tr>
@@ -73,7 +112,6 @@ export default function HistoryView() {
               <tr key={item._id}>
                 <td>{item.asin}</td>
                 <td>{item.sku}</td>
-                {/* <td>${item.price.toFixed(2)}</td> */}
                 <td>
                   {new Date(item.startDate).toLocaleString()}{" "}
                   <p style={{ color: "green" }}>Changed Price: ${item.price}</p>
@@ -83,20 +121,20 @@ export default function HistoryView() {
                     <>
                       {new Date(item.endDate).toLocaleString()}
                       {item.currentPrice && (
-                        <p style={{ margin: 0,color: "green" }}>
+                        <p style={{ margin: 0, color: "green" }}>
                           Reverted Price: ${item.currentPrice}
                         </p>
                       )}
                     </>
                   ) : (
-                    "Until Changed"
+                    <span style={{color:"red"}}>Until Changed</span>
                   )}
                 </td>
               </tr>
             ))
           ) : (
             <tr>
-              <td colSpan="5" className="text-center">
+              <td colSpan="4" className="text-center">
                 No data found
               </td>
             </tr>
