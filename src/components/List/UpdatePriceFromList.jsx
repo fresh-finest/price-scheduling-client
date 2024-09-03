370924b16779853916e509b06aec52a8df256b6f
fresh-finest <bb@brecx.com>
2024-09-03 22:59:24 +0600

    fixed local

--- src/components/List/UpdatePriceFromList.jsx
+++ src/components/List/UpdatePriceFromList.jsx
@@ -7,8 +7,8 @@ import { PriceScheduleContext } from "../../contexts/PriceScheduleContext";
 import axios from "axios";
 import { useSelector } from "react-redux";
 
-// const BASE_URL = `https://quiet-stream-22437-07fa6bb134e0.herokuapp.com/http://100.26.185.72:3000`;
-const BASE_URL ='http://localhost:3000'
+const BASE_URL = `https://quiet-stream-22437-07fa6bb134e0.herokuapp.com/http://100.26.185.72:3000`;
+// const BASE_URL ='http://localhost:3000'
 const fetchProductDetails = async (asin) => {
   try {
     const response = await axios.get(`${BASE_URL}/product/${asin}`);
