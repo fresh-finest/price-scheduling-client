3d6eb8bba8d0a2c94753263e6955804eddbc2eef
fresh-finest <bb@brecx.com>
2024-10-18 08:56:32 -0400

    fix locality

--- src/components/List/ProductDetailView.jsx
+++ src/components/List/ProductDetailView.jsx
@@ -775,11 +775,11 @@ const ProductDetailView = ({
                                                   <div className="flex justify-center w-full gap-2 my-2 px-2 ">
                                                     <div className="w-full">
                                                       <h3 className="flex text-sm justify-between items-center bg-[#F5F5F5] rounded px-2 py-1">
-                                                        {addHoursToTime(
+                                                        {/* {addHoursToTime(
                                                           timeSlot?.startTime,
                                                           6
-                                                        )}
-                                                        {/* {convertToUserLocalTime(timeSlot?.startTime)} */}
+                                                        )} */}
+                                                        {convertToUserLocalTime(timeSlot?.startTime)}
                                                         <span className="bg-blue-500 text-white p-1 rounded-sm">
                                                           $
                                                           {parseFloat(
@@ -793,11 +793,11 @@ const ProductDetailView = ({
                                                     </span>
                                                     <div className="w-full">
                                                       <h3 className="flex text-sm justify-between items-center bg-[#F5F5F5] rounded px-2 py-1">
-                                                        {addHoursToTime(
+                                                        {/* {addHoursToTime(
                                                           timeSlot.endTime,
                                                           6
-                                                        )}
-                                                        {/* {convertToUserLocalTime(timeSlot?.endTime)} */}
+                                                        )} */}
+                                                        {convertToUserLocalTime(timeSlot?.endTime)}
                                                         {timeSlot.revertPrice ? (
                                                           <span className="bg-red-700 text-white p-1 rounded-sm">
                                                             $
